8fba0727926226e88cbf51de6499226390227714
Evan Weinman <esweinman@gmail.com>
2022-04-05 15:43:45 -0500

    created functions for creating new files onto hard drive and also reading hard drive files onto file

--- main.go
+++ main.go
@@ -1,8 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
 func main() {
-	cards := newDeck()
-	fmt.Println(cards.toString())
+	cards := newDeckFromFile("my")
+	cards.print()
+}
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+// convert byte slice file on hard drive int a deck
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err !=nil {
+		// Option #1 - log the error and return a call to newDeck()
+		// Option #2 - log the error and entirely quit the program
+	fmt.Println("Error:", err)
+	os.Exit(1)		
+	}
+
+	s := strings.Split(string(bs), ",") 
+	return deck(s) 
 }
